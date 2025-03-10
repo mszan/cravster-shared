2b49a8055e2c80fff950e1f6e60fdb448d3023b0
Dawid Mszanowski <dmszanowski@icloud.com>
2025-03-10 19:02:09 +0100

    Add shared eslint and prettier configs with npm

---
+++ config/prettier/index.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+    printWidth: 120,
+    tabWidth: 2,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+    singleQuote: false,
+}
+
+export default config
